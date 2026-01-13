eb0628bb687a55a647e6e0c6cb66160bb6a5dd0d
SETOMIE <setomieart@gmail.com>
2026-01-13 15:54:03 +0800

    kill me

---
+++ libft/ft_itoa.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asyeo <asyeo@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/29 09:07:12 by asyeo             #+#    #+#             */
+/*   Updated: 2025/11/18 12:28:46 by asyeo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+//#include <stdio.h>
+
+static int	numlen(long n)
+{
+	int	len;
+
+	len = 1;
+	while (n >= 10)
+	{
+		n /= 10;
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*str;
+	int		len;
+	long	num;
+
+	num = n;
+	if (n < 0)
+		num = -num;
+	len = numlen(num) + (n < 0);
+	str = ft_calloc(len + 1, sizeof(char));
+	if (!str)
+		return (NULL);
+	str[len] = '\0';
+	while (len > 0)
+	{
+		str[--len] = num % 10 + 48;
+		num /= 10;
+	}
+	if (n < 0)
+		str[0] = '-';
+	return (str);
+}
+/*
+int	main(void)
+{
+	
+}
+*/
